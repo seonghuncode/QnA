c2169d6efce07eb8f221d27bedd4c75882ba57fa
seonghuncode <gnos456@naver.com>
2022-03-05 17:33:17 +0900

    첫번째 화면 만들어주기

---
+++ src/main/java/qna/controller/HomeController.java
@@ -0,0 +1,35 @@
+package qna.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import qna.util.RequestFactory;
+
+
+
+//처음 화면에 404오류 페이지가 아닌 다른 페이지를 보여주기 위해 만들어 준다..
+@WebServlet("/")
+public class HomeController extends HttpServlet {
+	
+       
+   
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestFactory factory = new RequestFactory(request, response);
+		
+		factory.forwardToJsp("index");
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>인덱스</title>
+</head>
+<body>
+
+	<h1>인텍스 입니다.</h1>
+	<div class="">  <!-- 지금은 비워나도 된다 -->
+		<a href="/question/add">
+			<span>질문하러 가기</span>
+		</a>
+	
+	</div>
+	<!-- 첫번째 페이지에 localhost/만 했을때 404에러가 아닌 인텍스 페이지 보여주기 -->
+	<!-- 서버에서 -> 톰캣 서버.xml가서 path에서 다 지워뒥 ""만 남겨야 바로 뜬다 -->
+	
+</body>
+</html>
