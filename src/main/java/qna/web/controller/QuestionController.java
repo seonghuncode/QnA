913afef0c94de803d15363f3a51cec3ff372bece
seonghuncode <gnos456@naver.com>
2022-03-12 18:04:12 +0900

    다음 시간 이어서

--- src/main/java/qna/web/controller/QuestionController.java
+++ src/main/java/qna/web/controller/QuestionController.java
@@ -1,31 +1,60 @@
 package qna.web.controller;
 
 import qna.app.components.Controller;
+import qna.app.container.AppContainer;
 import qna.app.util.AppRequest;
 import qna.app.util.UriProcessor;
+import qna.web.service.QuestionService;
 
 public class QuestionController implements Controller {
+	
+	private QuestionService questionService;
 
 	@Override
 	public void autoWried() {
-		// TODO Auto-generated method stub
+		
+		questionService = AppContainer.questionService;  //서비스 연결
 		
 	}
 
 	@Override
 	public void getMapping(AppRequest appReq, UriProcessor uriProcessor) {
-		// TODO Auto-generated method stub
+		
+		//get() 에서는 showWrite(), showModify,, /  post()에서는 doWrite()......		
+		//getmapping의 기능을 비슷하게 만들어준 것이다.
+		switch(uriProcessor.getRequestUri()) {
+			
+		case "/qna/questions/add":
+			showWriter(appReq);
+			break;
+		
+		}
 		
 	}
 
 	@Override
 	public void postMapping(AppRequest appReq, UriProcessor uriProcessor) {
-		// TODO Auto-generated method stub
+		
+		switch( uriProcessor.getRequestUri() ) {
+		
+			case "/qna/questions/add":
+				doWrite(appReq);
+				break;
+		}
 		
 	}
+
 	
 	// --> 
 	//alt shif s -> override methode
+	
+	
+	public void showWrite(AppRequest appRequest) {
+		appRequest.forwardToJsp("usr/question/add");
+		
+	}
+	
+	
 	
 
 }
